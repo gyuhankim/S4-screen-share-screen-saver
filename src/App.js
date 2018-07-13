9855db651663380a1beaa4c3117466f36b683c8d
Joseph Kim <gyuhankim@gmail.com>
2018-07-12 23:18:15 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      hideMessage: true
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      hideMessage: false
+    })
+  }
+
+  handleInput(val) {
+    this.setState({
+      message: val
+    })
+  }
+
   render() {
+
+    if (this.state.hideMessage) {
+      return (
+        <div className="container">
+          <div className="s4-container">
+  
+            <form>
+              <input onChange={(val) => this.handleInput(val.target.value)}>
+              </input>
+              <button type="submit" onClick={(e) => this.handleSubmit(e)}>
+                Take a break
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="s4-container">
+        <div className="s4">
+          <a href="/">
+            {this.state.message}
+          </a>
+        </div>
       </div>
-    );
+    )
   }
 }
 
