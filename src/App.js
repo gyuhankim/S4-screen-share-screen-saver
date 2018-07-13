d0ed1757b188a733986fdf5ea8047d0a207bff10
Joseph Kim <gyuhankim@gmail.com>
2018-07-13 19:42:14 -0400

    skeleton code for themes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ class App extends Component {
     super(props);
     this.state = {
       message: '',
-      hideMessage: true
+      hideMessage: true,
+      themes: ['white', 'black', 'blue', 'orange', 'red'],
+      activeTheme: 'white'
     }
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    if (this.state.message) {
+      this.setState({
+        hideMessage: false
+      })
+    }
+  }
+
+  handleMessageClick() {
     this.setState({
-      hideMessage: false
+      hideMessage: true
     })
   }
 
@@ -23,20 +33,39 @@ class App extends Component {
     })
   }
 
+  handleThemeClick(val) {
+    this.setState({
+      activeTheme: val
+    })
+  }
+
   render() {
+
+    const buttons = this.state.themes.map((theme, index) => {
+      return (
+        <button key={index} className={theme} onClick={() => this.handleThemeClick(theme)}/>
+      )
+    })
 
     if (this.state.hideMessage) {
       return (
-        <div className="container">
-          <div className="s4-container">
+        <div className={`container`}>
+          <div className={`s4-container`}>
   
             <form>
-              <input onChange={(val) => this.handleInput(val.target.value)}>
+              <input onChange={(val) => this.handleInput(val.target.value)} required>
               </input>
               <button type="submit" onClick={(e) => this.handleSubmit(e)}>
-                Take a break
+                brb
               </button>
             </form>
+
+            <div className="theme-picker">
+
+              {buttons}
+
+            </div>
+
           </div>
         </div>
       );
@@ -44,7 +73,7 @@ class App extends Component {
     return (
       <div className="s4-container">
         <div className="s4">
-          <a href="/">
+          <a href="#" onClick={() => this.handleMessageClick()}>
             {this.state.message}
           </a>
         </div>
